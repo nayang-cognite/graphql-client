7b1985461529627f27851a7bbf25f625be01b961
Nancy Yang <nancy.yang@cognite.com>
2021-06-24 10:40:55 -0400

    initial checkin

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+  gql,
+  useQuery,
+   useMutation
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+   uri: "http://localhost:4000"
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+//
+// Operations
+//
+const QUERY = gql`
+query MyQuery {
+  Books {
+    title    
+    author
+  }
+}
+`
+
+const ADD_BOOK = gql`
+mutation AddBook($title:String!, $author:String!) {
+  addBook(title:$title, author:$author) {
+    title
+    author
+	}
+}
+`
+function AddBook() {
+  let TitleInput, AuthorInput;
+  const [addBook, { data }] = useMutation(ADD_BOOK);
+
+   console.log("@@ data")
+   console.log(data)
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+           console.log(TitleInput.value)
+           console.log(AuthorInput.value)
+          addBook({ variables: { title: TitleInput.value, author: AuthorInput.value } });
+          TitleInput.value = '';
+          AuthorInput.value = '';
+        }}
+      >
+        <input
+          ref={node => {
+            TitleInput = node;
+          }}
+        />
+        <input
+          ref={node => {
+            AuthorInput = node;
+          }}
+        />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+}
+
+function Books() {  
+  const {loading, error, data, refetch} = useQuery(QUERY)
+
+  if (loading) return <p>loading...</p>
+  
+  if (error) return <p>Error:...</p>
+
+  return (
+     <div>
+	     <ul>
+	      {data.Books.map((d,i)=> <li key={i}>{d.title} by {d.author}</li>)}
+	     </ul>
+        <button onClick={()=>refetch()}> Reload! </button>
+     </div>
+  )
+}
+
+function App() {
+
+  return (
+    <ApolloProvider client={client}>
+      <AddBook />
+      <Books />
+    </ApolloProvider>
+  );
+}
+
+export default App;
